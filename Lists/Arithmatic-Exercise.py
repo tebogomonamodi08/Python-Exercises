ed1c5b40b3a622819bd2776177f1cfe78534a93f
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2024-12-03 19:47:47 +0200

    updated arithmetic calculator, using functional programming

--- Lists/Arithmatic-Exercise.py
+++ Lists/Arithmatic-Exercise.py
@@ -1,48 +1,61 @@
-def verify_length(list_):
-    count = 0
-    for item in list_:
-        count += 1
-        if count > 5:
-            return False
-        else:
-            True
-
-def find_operants(equations):
-    '''This function find operator, number 1 annd number 2'''
-    for equation in equations:
-        if equation.find('-')!=-1:
-            operant = '-'
-            operant_index =equation.find('-')
-        elif equation.find('+')!=-1:
-            operant = '+'
-            operant_index = equation.find('+')
-        else:
-            print("Error: Operator must be '+' or '-'.")
-            
-        translate = str.maketrans({" ":""})
-        translated_equation = equation.translate(translate)
-        
-        number_1 = translated_equation[0:operant_index:]
-        number_2 = translated_equation[operant_index+1::]
-        
-        return number_1,operant,number_2
-
-           
-            
 def main():
-    equation_list = []
-    while True:
-        print('Enter an equation, in this format x +/ y:')
-        equation = input()
-        equation_list.append(equation)
-        if verify_length(equation_list)==False:
-            print('Error: Too many problems.')
-            break
-        find_operants(equations)
+    problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+    if verify_length(problems):
+        list_ = []
+        for equation in problems:
+            table = str.maketrans({" ":""})
+            formatted_equation = equation.translate(table)
+            operator, number_1, number_2 = find_operator(formatted_equation)
+            #list_.append(find_operator(formatted_equation))
+        #print(list_)
+    
+            if operant_verification(number_1,number_2):
+                list_.append(formatter(operator,number_1,number_2))
+            else:
+                print('Error: Numbers cannot be more than four digits.')
+    else:
+        print('Error: Too many problems.')
 
-equations = ['5453+243', '345-45']
-print(find_operants(equations))
 
+def verify_length(problems):
+    return len(problems)<=5
 
+def find_operator(equation):
+    '''Finds the operator in an equation and the operants, output: tuple'''
+    if equation.find('+')!=-1:
+        operator_index = equation.find('+')
+        operator = '+'
+    elif equation.find('-')!=-1:
+        operator_index = equation.find('-')
+        operator = '-'
+    else:
+        operator='Error: Operator must be '+' or '-'.'
+
+    number_1 = equation[0:operator_index:]
+    number_2 = equation[operator_index+1::]
+    
+    return operator, number_1, number_2
+
+def operant_verification(number_1,number_2):
+    return (len(number_1)<4 or len(number_2)<4) and (number_1.isdigit() and number_2.isdigit())
+
+def answer(operator, number_1, number_2):
+    int(number_1)
+    int(number_2)
+    add = lambda number_1,number_2 : number_1+number_2
+    subtract = lambda number_1, number_2 : number_1-number_2
+
+    if operator=="+":
+        return add(number_1,number_2)
+    else:
+        return subtract(number_1, number_2)
         
-        
+    
+
+def formatter(operator,number_1,number_2):
+    print(f'{number_1:>5}\n{operator}{number_2:>4}\n-----')
+    
+    
+
+if __name__ == '__main__':
+    main()
